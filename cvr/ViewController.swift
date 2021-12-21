ba01b4428af5908477f998b1d2cb3afd96188320
小洋粉 <l.7@qq.com>
2021-12-21 23:02:27 +0800

    Initial Commit

---
+++ cvr/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  cvr
+//
+//  Created by 小洋粉 on 2021/12/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
