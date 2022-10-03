d58dccc35b6db1937a61a4ad179ebcfbae0ffb09
小洋粉 <l.7@qq.com>
2022-10-03 22:19:54 +0800

    update

--- cvr/AppDelegate.swift
+++ cvr/AppDelegate.swift
@@ -4,7 +4,7 @@
 //
 //  Created by 小洋粉 on 2021/12/21.
 //
-
+import SwiftUI
 import Cocoa
 import HotKey
 import LaunchAtLogin
@@ -14,7 +14,9 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
     internal var hotKeys: [String: HotKey] = [
         "C+b" : HotKey(key: .b, modifiers: [.command]),
-        "C+g" : HotKey(key: .g, modifiers: [.command])
+        "C+g" : HotKey(key: .g, modifiers: [.command]),
+        "C+O+b" : HotKey(key: .b, modifiers: [.command,.option]),
+        "C+O+g" : HotKey(key: .g, modifiers: [.command,.option]),
     ]
     
     
@@ -40,11 +42,18 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
 }
 
+
+
+
 extension AppDelegate {
     func constructMenu() {
+
         let menu = NSMenu()
+  
         menu.addItem(NSMenuItem(title:"⌘+B一键百度", action: nil ,  keyEquivalent: ""))
         menu.addItem(NSMenuItem(title:"⌘+G一键谷歌", action: nil ,  keyEquivalent: ""))
+        menu.addItem(NSMenuItem(title:"⌘⌥+B一键百度翻译", action: nil ,  keyEquivalent: ""))
+        menu.addItem(NSMenuItem(title:"⌘⌥+G一键谷歌翻译", action: nil ,  keyEquivalent: ""))
         menu.addItem(NSMenuItem.separator())
         let str = LaunchAtLogin.isEnabled ? "✔" : "✗"
         menu.addItem(NSMenuItem(title:str + "  开机启动" , action: #selector(AppDelegate.startLogin(_:)), keyEquivalent: "s"))
@@ -65,15 +74,41 @@ extension AppDelegate  {
         hotKeys["C+b"]?.keyDownHandler = { [self] in
             commandCStr() { str in
                 let appStoreURL =  "https://www.baidu.com/s?ie=UTF-8&wd=\(str)"
-                    .addingPercentEncoding(withAllowedCharacters:.urlQueryAllowed)
-                NSWorkspace.shared.open(URL(string: appStoreURL ?? "https://www.baidu.com")!)
+                
+                NSWorkspace.shared.open(URL(string: appStoreURL )!)
             }
         }
         hotKeys["C+g"]?.keyDownHandler = { [self] in
             commandCStr() { str in
                 let appStoreURL =  "https://www.google.com/search?q=\(str)"
-                    .addingPercentEncoding(withAllowedCharacters:.urlQueryAllowed)
-                NSWorkspace.shared.open(URL(string: appStoreURL ?? "https://www.goolge.com")!)
+                
+                NSWorkspace.shared.open(URL(string: appStoreURL )!)
+            }
+        }
+        hotKeys["C+O+b"]?.keyDownHandler = { [self] in
+            commandCStr() { str in
+                let appStoreURL =  "https://fanyi.baidu.com/#en/zh/\(str)"
+                
+                NSWorkspace.shared.open(URL(string: appStoreURL )!)
+            }
+        }
+        hotKeys["C+O+g"]?.keyDownHandler = { [self] in
+            commandCStr() { str in
+                func isChinese(str: String) -> Bool{
+                    let match: String = "(^[\\u4e00-\\u9fa5]+$)"
+                    let predicate = NSPredicate(format: "SELF matches %@", match)
+                    return predicate.evaluate(with: str)
+                }
+                if(isChinese(str: str.removingPercentEncoding ?? "")) {
+                    let appStoreURL =  "https://translate.google.cn/?sl=zh-CN&tl=en&text=\(str)&op=translate"
+                    
+                    NSWorkspace.shared.open(URL(string: appStoreURL )!)
+                } else {
+                    let appStoreURL =  "https://translate.google.cn/?sl=en&tl=zh-CN&text=\(str)&op=translate"
+                    
+                    NSWorkspace.shared.open(URL(string: appStoreURL )!)
+                }
+                
             }
         }
     }
@@ -85,7 +120,8 @@ extension AppDelegate {
         // 模拟command+c按键
         analogButton(flags: [.maskCommand],key: 0x08)
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
-            perform(NSPasteboard.general.string(forType: NSPasteboard.PasteboardType.string) ?? "")
+            perform(NSPasteboard.general.string(forType: NSPasteboard.PasteboardType.string)?
+                        .addingPercentEncoding(withAllowedCharacters:.urlQueryAllowed) ?? "")
         }
     }
     
